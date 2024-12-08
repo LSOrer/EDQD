270aad7fb267b458153172b16e69c35c4e1da0f6
Merlin B <46399067+LSOrer@users.noreply.github.com>
2024-12-08 11:34:27 +0100

    update configurations to make the application usable by docker out-of-the-box

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def get_thresholds():
     return jsonify(thresholds)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=80, debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
